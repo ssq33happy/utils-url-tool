0c8361077357f5e25781ecd21aed4cac14ee8fe8
ssq <shiqiang.sun@tongdun.cn>
2018-08-09 12:58:23 +0800

    完成url功能实现，错误控制台打印，可视化文档，npm打包

---
+++ src/index.js
@@ -0,0 +1,176 @@
+/**
+ * URL处理封装
+ * @param config
+ */
+import TDLog4j from '@tongdun/utils-log4j'
+
+class TDUrl {
+  constructor() {
+    this.parseURL = this.parseURL.bind(this)
+    this.parseURLParams = this.parseURLParams.bind(this)
+    this.getURLParam = this.getURLParam.bind(this)
+    this.appendParam = this.appendParam.bind(this)
+    this.appendParams = this.appendParams.bind(this)
+    this.removeParams = this.removeParams.bind(this)
+    this.appendParams = this.appendParams.bind(this)
+    this.printErrorLog = this.printErrorLog.bind(this)
+  }
+
+  parseURL(url) { // 把url的所有信息转化成json对象
+    try {
+      let a = document.createElement('a')
+      a.href = url || window.location.href
+      return {
+        source: url,
+        protocol: a.protocol.replace(':', ''),
+        hostname: a.hostname,
+        port: a.port,
+        search: a.search,
+        origin: a.origin,
+        params: (function () {
+          let ret = {}
+          let seg = a.search.replace(/^\?/, '').split('&')
+          let len = seg.length
+          let i = 0
+          let s
+          for (; i < len; i++) {
+            if (!seg[i]) {
+              continue
+            }
+            s = seg[i].split('=')
+            ret[s[0]] = s[1]
+          }
+          return ret
+        })(),
+        file: (a.pathname.match(/\/([^\/?#]+)$/i) || ['', ''])[1],
+        hash: a.hash,
+        hashName: a.hash.replace('#', ''),
+        pathname: a.pathname,
+        originPath: a.origin + a.pathname,
+        relative: (a.href.match(/tps?:\/\/[^\/]+(.+)/) || ['', ''])[1],
+        segments: a.pathname.replace(/^\//, '').split('/')
+      }
+    } catch (e) {
+      this.printErrorLog(e)
+      return {}
+    }
+  }
+
+  parseURLParams(url) { // 把url的参数部分转化成json对象
+    try {
+      let a = document.createElement('a')
+      a.href = url || window.location.href
+      let ret = {}
+      let seg = a.search.replace(/^\?/, '').split('&')
+      let len = seg.length
+      let i = 0
+      let s
+      for (; i < len; i++) {
+        if (!seg[i]) {
+          continue
+        }
+        s = seg[i].split('=')
+        ret[s[0]] = s[1]
+      }
+      return ret
+    } catch (e) {
+      this.printErrorLog(e)
+      return {}
+    }
+  }
+
+  getURLParam(key, url) { // 通过key获取url中的参数值
+    try {
+      let a = document.createElement('a')
+      a.href = url || window.location.href
+      let reg = new RegExp('[?&]' + key + '=([^&]+)')
+      return a.search.match(reg) ? RegExp.$1 : ''
+    } catch (e) {
+      this.printErrorLog(e)
+      return ''
+    }
+  }
+
+  appendParam(key, value, url) { // 向url中添加字段
+    !this.typeCheck.isString(key) && this.printErrorLog({ name: 'appendParam', message: '参数key非字符类型' })
+    this.typeCheck.isObject(value) && this.printErrorLog({ name: 'appendParam', message: '参数value不能是对象类型' })
+    if (!this.typeCheck.isString(key) || this.typeCheck.isObject(value)) return ''
+    return this.appendParams({ [key]: value }, url)
+  }
+
+  appendParams(params, url) { // 向url中添加多个字段
+    try {
+      !this.typeCheck.isObject(params) && this.printErrorLog({ name: 'appendParams', message: '参数params非json对象' })
+      let a = document.createElement('a')
+      a.href = url || window.location.href
+      let temp = this.parseURL(url)
+      let originPath = temp.originPath
+      let search = '?'
+      params = { ...temp.params, ...params }
+      for (let i in params) {
+        search += i + '=' + params[i] + '&'
+      }
+      search = search === '?' ? '' : search.substring(0, search.length - 1)
+      return originPath + search + temp.hash
+    } catch (e) {
+      this.printErrorLog(e)
+      return ''
+    }
+  }
+
+  removeParams(arr, url) { // 从url中去除多个字段
+    try {
+      !this.typeCheck.isArray(arr) && this.printErrorLog({ name: 'removeParams', message: '参数arr非数组' })
+      if (this.typeCheck.isArray(arr)) {
+        if (arr.findIndex(item => !this.typeCheck.isString(item)) !== -1) {
+          this.printErrorLog({ name: 'removeParams', message: '参数arr数组元素有非字符元素' })
+          return url
+        }
+      }
+      let temp = this.parseURL(url)
+      arr.forEach((item) => {
+        delete temp.params[item]
+      })
+      return this.appendParams(temp.params, temp.originPath)
+    } catch (e) {
+      this.printErrorLog(e)
+      return ''
+    }
+  }
+
+  printErrorLog(e) { // 打印参数日志
+    TDLog4j.error('error name:', e.name)
+    TDLog4j.error('error message:', e.message)
+    TDLog4j.error('error description:', e.description)
+  }
+}
+
+TDUrl.prototype.typeCheck = (function () { // 类型校验
+  let __isType = function (type) {
+    return function (obj) {
+      return Object.prototype.toString.call(obj) === '[object ' + type + ']'
+    }
+  }
+  return {
+    isObject: function (obj) {
+      return __isType('Object')(obj)
+    },
+    isArray: function (obj) {
+      return Array.isArray(obj) || __isType('Array')(obj)
+    },
+    isString: function (obj) {
+      return __isType('String')(obj)
+    },
+    isUndefined: function (obj) {
+      return __isType('Undefined')(obj)
+    },
+    isNumber: function (obj) {
+      return __isType('Number')(obj) && isFinite(obj) && !isNaN(obj)
+    },
+    isNullOrUndefined: function (obj) {
+      return (obj === undefined || obj === null)
+    }
+  }
+}())
+
+export default new TDUrl()
